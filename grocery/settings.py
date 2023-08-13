25c4aefd5ebc0fd5a91b7c604af77b4aa90097e6
Srijanomar3094 <srijanomar5840@gmail.com>
2023-08-14 01:22:27 +0530

    third commit

--- grocery/settings.py
+++ grocery/settings.py
@@ -39,7 +39,7 @@ SECURE_SSL_REDIRECT = True
 
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
 
-SESSION_ENGINE = "django.contribxx.sessions.backends.signed_cookies"
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
 
 
@@ -155,23 +155,7 @@ USE_L10N = True
 USE_TZ = False
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
-
-# SESSION_COOKIE_SECURE = True
-# SESSION_COOKIE_SAMESITE = 'None'
-# SESSION_COOKIE_HTTPONLY = False
-
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
